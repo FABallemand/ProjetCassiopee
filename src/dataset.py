69d56b00903e3ee6e9874dd6ef9049d78ac80955
FABallemand <allemand.fabien@orange.fr>
2024-03-04 16:20:17 +0100

    Update Dataset: - Add support for depth - Add support for mask - Add support for location

--- src/dataset.py
+++ src/dataset.py
@@ -1,4 +1,6 @@
 import os
+import numpy as np
+import cv2
 import torch
 import torchvision
 from torch.utils.data import Dataset, DataLoader
@@ -9,12 +11,16 @@ from torch.utils.data import Dataset, DataLoader
 
 class RGBDObjectDataset(Dataset):
 
-    def __init__(self, path, train):
+    def __init__(self, path, mode, modalities=["rgb"], transform=None, nb_imgs=None):
         super().__init__()
         self.path = path
 
-        self.train = train
+        self.mode = mode
+        self.modalities = modalities
         self.train_test_ratio = 4
+        self.transform = transform
+        self.nb_imgs = nb_imgs
+
         self.class_dict = None
 
         self.x = []
@@ -23,17 +29,53 @@ class RGBDObjectDataset(Dataset):
         self.create_labels_dict()
         self.load_data()
 
+        if nb_imgs is not None:
+            self.x = self.x[:nb_imgs]
+            self.y = self.y[:nb_imgs]
+
     def __len__(self):
         return len(self.y)
     
     def __getitem__(self, idx):
-        img_path = os.path.join(self.path,
-                                "_".join(self.x[idx].split("_")[:-3]),
-                                "_".join(self.x[idx].split("_")[:-2]),
-                                self.x[idx])
-        img = torchvision.io.read_image(img_path)
+        data_path = os.path.join(self.path,
+                                 "_".join(self.x[idx].split("_")[:-3]),
+                                 "_".join(self.x[idx].split("_")[:-2]),
+                                 self.x[idx])
+
+        load_transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
+
+        # RGB Data
+        img = -1
+        if "rgb" in self.modalities:
+            rgb = cv2.imread(data_path + ".png")
+            rgb = load_transform(rgb)
+            if self.transform:
+                rgb = self.transform(img)
+
+        # Depth Data
+        depth = -1
+        if "depth" in self.modalities:
+            depth = cv2.imread(data_path + "_depth.png")
+            depth = load_transform(depth)
+
+        # Mask Data
+        mask = -1
+        if "mask" in self.modalities:
+            mask = cv2.imread(data_path + "_mask.png")
+            mask = load_transform(mask)
+        
+        # Location Data
+        loc = -1
+        if "loc" in self.modalities:
+            with open(data_path + "_loc.txt", "r") as loc_file:
+                loc_x, loc_y = loc_file.readlines()[0].split(",")
+                loc_x = int(loc_x)
+                loc_y = int(loc_y)
+
+        # Label
         label = self.y[idx]
-        return img, label
+
+        return rgb, depth, mask, loc_x, loc_y, label
     
     def create_labels_dict(self):
         classes = os.listdir(self.path)
@@ -46,16 +88,18 @@ class RGBDObjectDataset(Dataset):
         disc = ["loc", "depth", "mask"]
         for c in list(self.class_dict.keys()):
             for sc in os.listdir(os.path.join(self.path, c)):
-                rgb_imgs = [f for f in os.listdir(os.path.join(self.path, c, sc)) if not any(d in f for d in disc)]
+                data = [f[:-4] for f in os.listdir(os.path.join(self.path, c, sc)) if not any(d in f for d in disc)]
                 
                 nb_new = 0
-                if self.train:
-                    new = [img for img in rgb_imgs if int(img.split("_")[-1][:-4]) % self.train_test_ratio != 0]
+                if self.mode == "train":
+                    new = [img for img in data if int(img.split("_")[-1]) % self.train_test_ratio != 0]
+                    nb_new = len(new)
+                    self.x += new
+                elif self.mode == "test":
+                    new = [img for img in data if int(img.split("_")[-1]) % self.train_test_ratio == 0]
                     nb_new = len(new)
                     self.x += new
                 else:
-                    new = [img for img in rgb_imgs if int(img.split("_")[-1][:-4]) % self.train_test_ratio == 0]
-                    nb_new = len(new)
-                    self.x += new
+                    print(f"ERROR: Invalid dataset mode {self.mode}")
 
                 self.y += [self.class_dict[c]] * nb_new
